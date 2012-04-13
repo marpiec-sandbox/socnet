bdb8d46ad3aa48ad7e792c7e1e152424d44ab78d
marpiec <m.pieciukiewicz@gmail.com>
2012-04-13 21:52:11 +0200

    Gson Option, LocalDateTime and LocalDate serialization fixed

--- util/src/main/scala/pl/marpiec/util/JsonUtil.scala
+++ util/src/main/scala/pl/marpiec/util/JsonUtil.scala
@@ -1,27 +1,45 @@
 package pl.marpiec.util
 
-import java.lang.reflect.Type
 import com.google.gson._
 import java.util.Date
-import org.joda.time.{Instant, LocalDateTime, DateTime}
+import org.joda.time._
+import java.lang.reflect.{ParameterizedType, Type}
 
 /**
  * @author Marcin Pieciukiewicz
  */
 
-class DateTimeTypeConverter extends JsonSerializer[LocalDateTime] with JsonDeserializer[LocalDateTime] {
+class LocalDateTimeTypeConverter extends JsonSerializer[LocalDateTime] with JsonDeserializer[LocalDateTime] {
   def serialize(src: LocalDateTime, typeOfSrc: Type, context: JsonSerializationContext) = {
-    new JsonPrimitive(src.toString());
+    new JsonPrimitive(src.toString("yyyy-MM-dd HH:mm:ss:SSS"));
   }
 
   def deserialize(json: JsonElement, typeOfT: Type, context: JsonDeserializationContext) = {
     try {
       new LocalDateTime(json.getAsString());
     } catch {
-      case e:Exception  => {
+      case e: Exception => {
         // May be it came in formatted as a java.util.Date, so try that
-        val date:Date = context.deserialize(json, classOf[Date]);
+        val date: Date = context.deserialize(json, classOf[Date]);
         new LocalDateTime(date);
+      }
+    }
+  }
+}
+
+class LocalDateTypeConverter extends JsonSerializer[LocalDate] with JsonDeserializer[LocalDate] {
+  def serialize(src: LocalDate, typeOfSrc: Type, context: JsonSerializationContext) = {
+    new JsonPrimitive(src.toString("yyyy-MM-dd"));
+  }
+
+  def deserialize(json: JsonElement, typeOfT: Type, context: JsonDeserializationContext) = {
+    try {
+      new LocalDate(json.getAsString());
+    } catch {
+      case e: Exception => {
+        // May be it came in formatted as a java.util.Date, so try that
+        val date: Date = context.deserialize(json, classOf[Date]);
+        new LocalDate(date);
       }
     }
   }
@@ -38,47 +56,47 @@ class InstantTypeConverter extends JsonSerializer[Instant] with JsonDeserializer
 }
 
 
-class OptionSerializer extends JsonSerializer[Option[Any]] with JsonDeserializer[Option[Any]]  {
+class OptionSerializer extends JsonSerializer[Option[Any]] with JsonDeserializer[Option[Any]] {
   def serialize(src: Option[Any], typeOfSrc: Type, context: JsonSerializationContext): JsonElement = {
-    new JsonPrimitive(src.getOrElse("none").toString());
+    if (src.isDefined) {
+      context.serialize(src.get);
+    } else {
+      JsonNull.INSTANCE
+    }
+
   }
 
   def deserialize(json: JsonElement, typeOfT: Type, context: JsonDeserializationContext) = {
-    var jsonObject = json.getAsJsonObject
-
-    if(jsonObject.entrySet().isEmpty) {
+    if (json.isJsonNull) {
       None
     } else {
-      throw new IllegalStateException("Option deserialization Not yet implemented")
+      val arguments = typeOfT.asInstanceOf[ParameterizedType].getActualTypeArguments
+      Option(context.deserialize(json, arguments(0)))
     }
-    
   }
 }
-
-
 
 
 class JsonUtil {
 
   val gson = buildGson
-  
-  private def buildGson:Gson = {
+
+  private def buildGson: Gson = {
     val gsonBuilder = new GsonBuilder
-    gsonBuilder.registerTypeAdapter(classOf[LocalDateTime], new DateTimeTypeConverter)
+    gsonBuilder.registerTypeAdapter(classOf[LocalDateTime], new LocalDateTimeTypeConverter)
+    gsonBuilder.registerTypeAdapter(classOf[LocalDate], new LocalDateTypeConverter)
     gsonBuilder.registerTypeAdapter(classOf[Instant], new InstantTypeConverter)
     gsonBuilder.registerTypeAdapter(classOf[Option[Any]], new OptionSerializer)
     gsonBuilder.create
   }
-  
-  def toJson(obj:AnyRef):String = {
+
+  def toJson(obj: AnyRef): String = {
     gson.toJson(obj)
   }
-  
-  def fromJson[E](json:String, clazz: Class[E]):E = {
+
+  def fromJson[E](json: String, clazz: Class[E]): E = {
     gson.fromJson(json, clazz)
   }
 
 
-
-  
 }
