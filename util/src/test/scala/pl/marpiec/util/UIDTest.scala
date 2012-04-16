8524850d4cd902423ef8edd9db8ef3932f6cb4c2
Marcin Pieciukiewicz <m.pieciukiewicz@gmail.com>
2012-04-16 10:17:39 +0200

    UID test added

---
+++ util/src/test/scala/pl/marpiec/util/UIDTest.scala
@@ -0,0 +1,35 @@
+package pl.marpiec.util
+
+import org.testng.annotations.Test
+import org.testng.Assert._
+/**
+ * @author Marcin Pieciukiewicz
+ */
+
+@Test
+class UIDTest {
+  
+  def testSimpleUIDUsage() {
+    val a = new UID(5)
+    val b = new UID(10)
+    val c = new UID(5)
+    
+    assertNotEquals(a, b)
+    assertNotEquals(b, c)
+    assertEquals(a, c)
+    assertEquals(a.hashCode(), c.hashCode())
+
+    assertNotEquals(a, 5L)
+    
+    var d = a.nextUid
+    
+    assertEquals(d.uid, 6)
+    
+    val e = UID.parseOrZero("35")
+    assertEquals(e, new UID(35))
+
+    val f = UID.parseOrZero("s2")
+    assertEquals(f, new UID(0))
+
+  }
+}
