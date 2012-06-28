8dab2353aebeef58a5f475a5c823ab3c138d0ea0
Marcin Pieciukiewicz <m.pieciukiewicz@gmail.com>
2012-06-28 11:12:40 +0200

    Easy way to add single ajax button secured with session token

---
+++ webapp/src/main/scala/pl/marpiec/socnet/web/component/wicket/form/OneButtonAjaxForm.scala
@@ -0,0 +1,42 @@
+package pl.marpiec.socnet.web.component.wicket.form
+
+import org.apache.wicket.ajax.AjaxRequestTarget
+import org.apache.wicket.model.CompoundPropertyModel
+import pl.marpiec.socnet.web.application.SocnetSession
+import org.apache.wicket.markup.html.form.{HiddenField, Form}
+import org.apache.wicket.markup.html.panel.Panel
+import pl.marpiec.socnet.web.wicket.{SecureAjaxButton, SecureFormModel}
+import org.apache.wicket.markup.html.basic.Label
+
+/**
+ * @author Marcin Pieciukiewicz
+ */
+
+class OneButtonAjaxForm(id: String, buttonLabel: String, onClick: (AjaxRequestTarget) => Unit) extends Panel(id) {
+
+  add(new Form[SecureFormModel]("form") {
+
+    setModel(new CompoundPropertyModel[SecureFormModel](new SecureFormModel))
+
+    add(new HiddenField[String]("sessionToken"))
+
+    override def onInitialize() {
+      super.onInitialize()
+
+      getModel.asInstanceOf[CompoundPropertyModel[SecureFormModel]].getObject.sessionToken =
+        getSession.asInstanceOf[SocnetSession].sessionToken
+    }
+
+    add(new SecureAjaxButton[SecureFormModel]("button") {
+      add(new Label("label", buttonLabel))
+
+      def onSecureSubmit(target: AjaxRequestTarget, formModel: SecureFormModel) {
+        onClick(target)
+      }
+    })
+
+
+  })
+
+}
+
