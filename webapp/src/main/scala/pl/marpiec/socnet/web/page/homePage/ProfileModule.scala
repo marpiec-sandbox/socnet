f5495f9cb4863f3687114e2b756589d6e03012a3
Marcin Pieciukiewicz <m.pieciukiewicz@gmail.com>
2012-08-30 17:27:46 +0200

    Some minor changes to home page

--- webapp/src/main/scala/pl/marpiec/socnet/web/page/homePage/ProfileModule.scala
+++ webapp/src/main/scala/pl/marpiec/socnet/web/page/homePage/ProfileModule.scala
@@ -5,6 +5,7 @@ import org.apache.wicket.markup.html.link.BookmarkablePageLink
 import pl.marpiec.socnet.web.page.profile.{UserProfilePreviewPage, EditUserProfilePage}
 import pl.marpiec.socnet.web.authorization.AuthorizeUser
 import pl.marpiec.socnet.web.application.SocnetSession
+import org.apache.wicket.markup.html.basic.Label
 
 /**
  * @author Marcin Pieciukiewicz
@@ -15,9 +16,16 @@ class ProfileModule(id:String) extends Panel(id) {
   val session = getSession.asInstanceOf[SocnetSession]
 
   if(session.isAuthenticated) {
+
+    add(new Label("userName", session.user.fullName))
+    add(new Label("professionalTitle", session.user.summary))
+
     add(AuthorizeUser(new BookmarkablePageLink("editProfileLink", classOf[EditUserProfilePage])))
     add(AuthorizeUser(UserProfilePreviewPage.getLink(session.user)))
   }
+
+
+
 
 
 }
