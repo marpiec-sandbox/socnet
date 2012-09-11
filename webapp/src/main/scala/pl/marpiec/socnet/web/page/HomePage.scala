edac705db70d7fee9b74da973957669043ac8ba7
Marcin Pieciukiewicz <m.pieciukiewicz@gmail.com>
2012-09-11 14:32:53 +0200

    Home page look and feel improved

--- webapp/src/main/scala/pl/marpiec/socnet/web/page/HomePage.scala
+++ webapp/src/main/scala/pl/marpiec/socnet/web/page/HomePage.scala
@@ -1,40 +1,28 @@
 package pl.marpiec.socnet.web.page
 
-import books.BooksPage
 import homePage._
-import homePage.people.PeopleDashboardPanel
 import org.apache.wicket.markup.html.link.BookmarkablePageLink
-import pl.marpiec.socnet.readdatabase.ArticleDatabase
 import pl.marpiec.socnet.web.authorization.{UnauthorizeAll, AuthorizeUser}
 import pl.marpiec.socnet.web.page.template.SimpleTemplatePage
 import org.apache.wicket.markup.html.WebMarkupContainer
 import registration.RegisterPage
 import signin.SignInFormPanel
-import org.apache.wicket.spring.injection.annot.SpringBean
-import usertechnologies.UserTechnologiesPage
 
 class HomePage extends SimpleTemplatePage {
 
-
-
   setStatelessHint(true)
-
-
-
-
-
 
   add(UnauthorizeAll(new WebMarkupContainer("loginPanel") {
     add(UnauthorizeAll(new SignInFormPanel("signInPanel")))
     add(UnauthorizeAll(new BookmarkablePageLink("registerLink", classOf[RegisterPage])))
   }))
 
-  add(new ArticlesModule("articlesModule"))
-  add(new BooksModule("booksModule"))
-  add(new LinksModule("linksModule"))
+  add(AuthorizeUser(new ArticlesModule("articlesModule")))
+  add(AuthorizeUser(new BooksModule("booksModule")))
+  add(AuthorizeUser(new LinksModule("linksModule")))
   add(AuthorizeUser(new MessagesModule("messagesModule")))
-  add(new PeopleModule("peopleModule"))
+  add(AuthorizeUser(new PeopleModule("peopleModule")))
   add(AuthorizeUser(new ProfileModule("profileModule")))
-  add(new TechnologiesModule("technologiesModule"))
+  add(AuthorizeUser(new TechnologiesModule("technologiesModule")))
 
 }
--- webapp/src/main/scala/pl/marpiec/socnet/web/page/homePage/ArticleList.scala
+++ webapp/src/main/scala/pl/marpiec/socnet/web/page/homePage/ArticleList.scala
@@ -15,23 +15,24 @@ import pl.marpiec.socnet.web.page.ArticlePage
 
 class ArticleList(id: String, private val articleList: List[Article]) extends Panel(id) {
 
-  add(new Label("title", "Lista artykulow (mamy obecnie " + articleList.length + " artykulow)"));
-
   val repeating: RepeatingView = new RepeatingView("repeating");
   add(repeating);
 
-  for (article <- articleList) {
+  addArticlesSummary(repeating, articleList)
 
-    val item: AbstractItem = new AbstractItem(repeating.newChildId());
-    repeating.add(item);
+  private def addArticlesSummary(repeating: RepeatingView, articleList: List[Article]) {
+    for (article <- articleList) {
 
-    item.add(new Label("content", article.content))
-    item.add(new Label("time", article.creationTime.toString))
-    item.add(new Label("author", article.authorUserId.toString))
-    val parameters: PageParameters = new PageParameters()
-    parameters.set(ArticlePage.ARTICLE_ID_PARAM, article.id)
+      val item: AbstractItem = new AbstractItem(repeating.newChildId());
+      repeating.add(item);
 
-    item.add(new BookmarkablePageLink("articleLink", classOf[ArticlePage], parameters))
+      item.add(new Label("title", "Lorem ipsum noran ot nevo"))
+      item.add(new Label("articleSummaryText", article.content))
+      item.add(new Label("author", article.authorUserId.toString))
+      val parameters: PageParameters = new PageParameters()
+      parameters.set(ArticlePage.ARTICLE_ID_PARAM, article.id)
+
+      item.add(new BookmarkablePageLink("articleLink", classOf[ArticlePage], parameters))
+    }
   }
-
 }
