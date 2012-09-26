d0e82d6235ad0fc72d8e39d964d3a28525657902
Marcin Pieciukiewicz <m.pieciukiewicz@gmail.com>
2012-09-26 12:53:05 +0200

    Json code reorganization

--- util/src/main/scala/pl/marpiec/util/mpjson/util/ObjectConstructionUtil.scala
+++ util/src/main/scala/pl/marpiec/util/mpjson/util/ObjectConstructionUtil.scala
@@ -1,7 +1,7 @@
 package pl.marpiec.util.mpjson.util
 
 import scala.Array
-import java.lang.reflect.{Field, Constructor}
+import java.lang.reflect.Field
 
 /**
  * @author Marcin Pieciukiewicz
@@ -16,32 +16,22 @@ object ObjectConstructionUtil {
   }
 
   def createInstance(clazz: Class[_]): AnyRef = {
-
-    val someConstructor: Constructor[_] = getPreferablyDefaultConstructor(clazz)
-    val argsCount = someConstructor.getParameterTypes.size
-
-    if (argsCount == 0) {
-      return someConstructor.newInstance().asInstanceOf[AnyRef]
-    } else {
-      createInstanceWithoutCallingConstructor(clazz)
-    }
-
-  }
-
-  private def getPreferablyDefaultConstructor(clazz: Class[_]): Constructor[_] = {
     try {
-      clazz.getConstructor() // try to get default constructor
+      // try to create object using default constructor
+      clazz.getConstructor().newInstance().asInstanceOf[AnyRef]
     } catch {
-      case e: NoSuchMethodException => clazz.getConstructors()(0) //otherwise get first constructor
+      //otherwise create object without calling constructor
+      case e: NoSuchMethodException => createInstanceWithoutCallingConstructor(clazz)
     }
   }
+
 
   private def getPreparedUnsafeObject: sun.misc.Unsafe = {
     try {
       val unsafeClass: Class[_] = classOf[sun.misc.Unsafe]
-      val fiels: Field = unsafeClass.getDeclaredField("theUnsafe")
-      fiels.setAccessible(true)
-      fiels.get(null).asInstanceOf[sun.misc.Unsafe]
+      val theUnsafeField: Field = unsafeClass.getDeclaredField("theUnsafe")
+      theUnsafeField.setAccessible(true)
+      theUnsafeField.get(null).asInstanceOf[sun.misc.Unsafe]
     } catch {
       case e: Exception => throw new IllegalStateException("Cannot create object without calling constructor\n" +
         "Thats probably because JRE is not sun/oracle implementation", e);
