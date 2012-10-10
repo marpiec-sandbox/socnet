0be7e18f5394f8e5bef96c469dd18fc8873b05fc
marpiec <m.pieciukiewicz@gmail.com>
2012-10-10 19:51:55 +0200

    Id protection util added

---
+++ util/src/main/scala/pl/marpiec/util/IdProtectionUtil.scala
@@ -0,0 +1,12 @@
+package pl.marpiec.util
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Marcin
+ * Date: 10.10.12
+ * Time: 18:35
+ * To change this template use File | Settings | File Templates.
+ */
+object IdProtectionUtil {
+
+}
