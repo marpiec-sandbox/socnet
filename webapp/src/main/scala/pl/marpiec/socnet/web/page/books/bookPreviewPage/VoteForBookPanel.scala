f8e2ff042b0478a2fc901d152beda05f69f240f2
Marcin Pieciukiewicz <m.pieciukiewicz@gmail.com>
2012-10-24 10:23:22 +0200

    A little change in process of voting for book

--- webapp/src/main/scala/pl/marpiec/socnet/web/page/books/bookPreviewPage/VoteForBookPanel.scala
+++ webapp/src/main/scala/pl/marpiec/socnet/web/page/books/bookPreviewPage/VoteForBookPanel.scala
@@ -1,18 +1,19 @@
 package pl.marpiec.socnet.web.page.books.bookPreviewPage
 
+import model.VoteForBookFormModel
 import scala.collection.JavaConversions._
 import org.apache.wicket.markup.html.panel.Panel
 import org.apache.wicket.spring.injection.annot.SpringBean
 import pl.marpiec.socnet.service.bookuserinfo.BookUserInfoCommand
 import pl.marpiec.socnet.constant.Rating
-import org.apache.wicket.model.Model
-import org.apache.wicket.markup.html.form.{ChoiceRenderer, DropDownChoice}
-import org.apache.wicket.ajax.form.AjaxFormComponentUpdatingBehavior
 import org.apache.wicket.ajax.AjaxRequestTarget
 import pl.marpiec.cqrs.AggregatesUtil
 import pl.marpiec.socnet.web.application.SocnetSession
 import pl.marpiec.socnet.model.BookUserInfo
 import pl.marpiec.socnet.web.page.books.BookPreviewPage
+import pl.marpiec.socnet.web.component.wicket.form.StandardAjaxSecureForm
+import org.apache.wicket.model.CompoundPropertyModel
+import org.apache.wicket.markup.html.form.{ChoiceRenderer, DropDownChoice}
 
 /**
  * @author Marcin Pieciukiewicz
@@ -25,25 +26,37 @@ class VoteForBookPanel(id: String,
 
   @SpringBean private var bookUserInfoCommand: BookUserInfoCommand = _
 
-  val session = getSession.asInstanceOf[SocnetSession]
+  val currentUserId = getSession.asInstanceOf[SocnetSession].userId
 
-  add(new DropDownChoice[Rating]("userBookRating",
-    new Model[Rating](previousUserBookRatingOption.getOrElse(null)), Rating.values,
-    new ChoiceRenderer[Rating]("translation")).add(new AjaxFormComponentUpdatingBehavior("onchange") {
-    def onUpdate(target: AjaxRequestTarget) {
-      val rating = this.getFormComponent.getModel.getObject.asInstanceOf[Rating]
-      val ratingOption = Option(rating)
+
+  add(new StandardAjaxSecureForm[VoteForBookFormModel]("voteForBookForm") {
+
+    def initialize = {
+      standardCancelButton = false
+      setModel(new CompoundPropertyModel[VoteForBookFormModel](new VoteForBookFormModel))
+    }
+
+    def buildSchema = {
+      add(new DropDownChoice[Rating]("userBookRating", Rating.values, new ChoiceRenderer[Rating]("translation")))
+    }
+
+    def onSecureSubmit(target: AjaxRequestTarget, formModel: VoteForBookFormModel) {
+      val ratingOption = Option(formModel.userBookRating)
 
       if (ratingOption.isDefined) {
-        bookUserInfoCommand.voteForBook(session.userId, bookUserInfo.bookId, bookUserInfo, rating)
+        bookUserInfoCommand.voteForBook(currentUserId, bookUserInfo.bookId, bookUserInfo, formModel.userBookRating)
       } else {
-        bookUserInfoCommand.cancelVoteForBook(session.userId, bookUserInfo.bookId, bookUserInfo)
+        bookUserInfoCommand.cancelVoteForBook(currentUserId, bookUserInfo.bookId, bookUserInfo)
       }
 
       AggregatesUtil.incrementVersion(bookUserInfo)
 
       parentPage.updateBookAverageRating(target, ratingOption)
     }
-  }))
+
+
+    def onSecureCancel(target: AjaxRequestTarget, formModel: VoteForBookFormModel) {}
+  })
+
 
 }
---
+++ webapp/src/main/scala/pl/marpiec/socnet/web/page/books/bookPreviewPage/model/VoteForBookFormModel.scala
@@ -0,0 +1,12 @@
+package pl.marpiec.socnet.web.page.books.bookPreviewPage.model
+
+import pl.marpiec.socnet.constant.Rating
+import pl.marpiec.socnet.web.wicket.SecureFormModel
+
+/**
+ * @author Marcin Pieciukiewicz
+ */
+
+class VoteForBookFormModel extends SecureFormModel {
+  var userBookRating:Rating = _
+}
